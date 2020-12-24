638282aad9b07fb809ef19fafdde39f0e9837e3d
LuisLiraC <luis_ssbm@hotmail.com>
2020-12-24 12:22:40 -0600

    data tests button and counter display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react'
+
 function App() {
   return (
     <div data-test='component-app'>
-      <h1>Asd</h1>
+      <h1 data-test='counter-display'>The counter is currently</h1>
+      <button data-test='increment-button'>Increment counter</button>
     </div>
   )
 }
